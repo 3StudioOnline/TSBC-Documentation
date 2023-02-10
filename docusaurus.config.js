cea5cc24cf1df83c513b8b30480e6761c37b216d
mohamad al moussawi <mohamadalmoussawi0@gmail.com>
2023-02-10 16:32:19 +0200

    2.0.1

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -55,7 +55,7 @@ const config = {
                     showLastUpdateTime: false,
                     includeCurrentVersion: false,
                     versions: {
-                        "2.0.0": {
+                        "2.0.1": {
                             banner: "none",
                         },
                         "1.2.0": {
