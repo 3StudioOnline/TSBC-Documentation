5d01280ff161682d29ab342dab03ba659a61e6ae
Andreas Oehlke <andreas.oehlke@3studio.online>
2023-09-02 17:14:45 +0200

    Init 2.2.x docs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -56,6 +56,9 @@ const config = {
                     showLastUpdateTime: false,
                     includeCurrentVersion: false,
                     versions: {
+                        "2.2.0": {
+                            banner: "none",
+                        },
                         "2.1.2": {
                             banner: "none",
                         },
