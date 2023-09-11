75cd2fea732d2c88bd946dde057fbe2cb0747246
Andreas Oehlke <andreas.oehlke@3studio.online>
2023-09-11 16:46:19 +0200

    update 2.2.1

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -56,7 +56,7 @@ const config = {
                     showLastUpdateTime: false,
                     includeCurrentVersion: false,
                     versions: {
-                        "2.2.0": {
+                        "2.2.1": {
                             banner: "none",
                         },
                         "2.1.2": {
