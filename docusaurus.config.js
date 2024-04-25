93959c464cd8df6474c8a3da966d54e271111492
Arden <Agata.Verkovskaja@3studio.online>
2024-04-25 15:18:05 +0300

    Update 2.6.0

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -55,6 +55,9 @@ const config = {
                     showLastUpdateTime: false,
                     includeCurrentVersion: false,
                     versions: {
+                        "2.6.0": {
+                            banner: "none",
+                        },
                         "2.5.1": {
                             banner: "none",
                         },
