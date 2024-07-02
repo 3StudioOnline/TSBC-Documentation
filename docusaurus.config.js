040a9ca7b422e38de998b81881a7111d23d22074
Andreas Oehlke <andreas.oehlke@3studio.online>
2024-07-02 14:57:39 +0200

    v3.1.1 #4276

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -55,7 +55,7 @@ const config = {
                     showLastUpdateTime: false,
                     includeCurrentVersion: false,
                     versions: {
-                        "3.1.0": {
+                        "3.1.1": {
                             banner: "none",
                         },
                         "3.0.0": {
